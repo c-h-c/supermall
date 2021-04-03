cf390252e10a476d5f41e2b9c23a57cc924b4945
halcyon <1350453635@qq.com>
2021-04-03 16:57:08 +0800

    second edit

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="goods">
+    <goods-list-item v-for="(item,index) in goods" :key="index" :goods-item = "item"/>
+    <ul>
+      <li>list1</li>
+      <li>list2</li>
+      <li>list3</li>
+      <li>list4</li>
+      <li>list5</li>
+      <li>list6</li>
+      <li>list7</li>
+      <li>list8</li>
+      <li>list9</li>
+      <li>list10</li>
+      <li>list1</li>
+      <li>list2</li>
+      <li>list3</li>
+      <li>list4</li>
+      <li>list5</li>
+      <li>list6</li>
+      <li>list7</li>
+      <li>list8</li>
+      <li>list9</li>
+      <li>list10</li>
+      <li>list1</li>
+      <li>list2</li>
+      <li>list3</li>
+      <li>list4</li>
+      <li>list5</li>
+      <li>list6</li>
+      <li>list7</li>
+      <li>list8</li>
+      <li>list9</li>
+      <li>list10</li>
+      <li>list1</li>
+      <li>list2</li>
+      <li>list3</li>
+      <li>list4</li>
+      <li>list5</li>
+      <li>list6</li>
+      <li>list7</li>
+      <li>list8</li>
+      <li>list9</li>
+      <li>list10</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import GoodsListItem from './GoodsListItem';
+
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props:{
+    goods:{
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  }
+}
+</script>
+<style>
+ .goods {
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: space-around;
+ }
+</style>
--- src/views/home/childComps/RecommendView.vue
+++ src/views/home/childComps/RecommendView.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="recommend">
-    <div v-for="(item,key) in recommends" :key="key" class="recommend-item">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
       <a :href="item.link">
         <img :src="item.image" alt="">
         <div>{{item.title}}</div>
@@ -19,32 +19,24 @@ export default {
         return []
       }
     }
-  },
-  components: {},
-  data() {
-    return {
-
-
-    };
-  },
   }
+}
 </script>
 <style scoped>
-  .recommend{
+  .recommend {
     display: flex;
+    width: 100%;
     text-align: center;
     font-size: 12px;
-
     padding: 10px 0 20px;
-    border-bottom: 10px solid #eee;
-
+	border-bottom: 10px solid #eee; 
   }
   .recommend-item {
     flex: 1;
   }
-  .recommend-item img{
-    width: 70px;
-    height: 70px;
+  .recommend-item img {
+    width: 60px;
+    height: 60px;
     margin-bottom: 10px;
-  }
+}
 </style>
